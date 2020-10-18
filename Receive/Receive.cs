94612eed5b9585fa20a492d047ae058a330ad564
Sameque Sant' ana <samequesantana@gmail.com>
2020-10-18 18:04:08 -0300

    initial

---
+++ Receive/Receive.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+class Receive
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        int index = 1;
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.BasicQos(0,1,false);
+/*
+            channel.QueueDeclare(queue: "person",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+*/
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    
+                    Thread.Sleep(2000);
+
+                    if (index < 4)
+                        Console.WriteLine(" [x] Received {0}", message);                        
+                    else 
+                        channel.Close();
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (System.Exception)
+                {
+                    channel.BasicNack(ea.DeliveryTag,false, true);
+                }
+
+
+            };
+            channel.BasicConsume(queue: "person",
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Send
+{
+    public static void Main()
+    {
+        ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+        int index = 1;
+
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "person",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+            channel.BasicQos(0,1,false);
+            while (true)
+            {
+                string message = $"message {index}";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                    routingKey: "person",
+                                    basicProperties: null,
+                                    body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+                index ++;
+                if (index > 100000) break;
+            }
+
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
